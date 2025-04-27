7012306e4f6fccbd9e1f479e4ce620d25dc2cc64
Felipe Rodrigues <me@felipefr.dev>
2025-04-27 01:49:25 -0300

    feat: add CHARACTER_REPOSITORY_TOKEN constant for character repository

--- src/modules/shared/domain/interfaces/character-repository.interface.ts
+++ src/modules/shared/domain/interfaces/character-repository.interface.ts
@@ -1,6 +1,9 @@
 import type { Character } from '../../../character/domain/entities/character'
 import { type CursorPaginationOptions, PaginatedResult } from '../../application/dtos/cursor-pagination.dto'
 
+/* istanbul ignore next */
+export const CHARACTER_REPOSITORY_TOKEN = 'CharacterRepositoryInterface'
+
 export interface CharacterRepositoryInterface {
   findById(id: string): Character | null
   findByName(name: string): Character | null
