417e47515e216760554a7a84da44a305b6379413
Felipe Rodrigues <me@felipefr.dev>
2025-04-27 01:19:24 -0300

    chore: remove unused error classes and battle repository interface

--- src/modules/shared/domain/interfaces/battle-repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { PaginatedResult } from '../../application/dtos/cursor-pagination.dto'
-import type { Battle } from '../models/battle'
-
-export interface BattleRepositoryInterface {
-  save(battle: Battle): Battle
-  findById(id: string): Battle | null
-  findAll(): PaginatedResult<Battle>
-}
