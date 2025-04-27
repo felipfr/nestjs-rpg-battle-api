bdae3723f2731e764660d553968b3ff063347c4b
Felipe Rodrigues <me@felipefr.dev>
2025-04-27 10:45:01 -0300

    feat: add SharedModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+
 import { BattleModule } from '~battle/battle.module'
 import { CharacterModule } from '~character/character.module'
+import { SharedModule } from '~shared/shared.module'
 
 @Module({
-  imports: [BattleModule, CharacterModule],
+  imports: [BattleModule, CharacterModule, SharedModule],
   controllers: [],
   providers: [],
 })
